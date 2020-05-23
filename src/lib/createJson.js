4b04f1ff44bcf1392f2c245c152460718355f130
Felipe Castillo <1280530+fcastilloec@users.noreply.github.com>
2020-05-22 20:08:39 -0700

    feat: add headless scrapping and saving to excel
    * Move chrome extension to its own folder
    
    * Set command line script
    
    * create command line app to process workfiles
    
    * Fix images not being part of the extension
    
    * Export an empty array instead of string for no authors
    
    * Save results as excel file
    
    * Add command-line option to save result as excel
    
    * feat: add headless scrapping and saving to excel
    
    * remove chrome extension but keep images for electron

--- src/lib/createJson.js
+++ src/lib/createJson.js
@@ -1,10 +1,8 @@
 /**
- * Watch for changes in the DOM before retrieving IEEE results and sends them to 'background.js'
- * The MutationObserver is the only way to see if the results have been loaded, since IEEE loads them via script.
- * Once all the "mutations" (a.k.a. changes to the DOM) have been seen, we send them via a port to the background
+ * Queries the document/page for IEEE results.
+ *
+ * @returns {object[]}  Each IEEE result is an Object, which are part of the returned Array
  */
-
-// Generates CSV data based on all the results
 function createJSON () {
   // LIST OF ELEMENTSS:
   const ELEMENTS = 'div.row.result-item.hide-mobile > div.col.result-item-align'
@@ -15,7 +13,6 @@ function createJSON () {
   const ABSTRACT = 'div.js-displayer-content.u-mt-1.stats-SearchResults_DocResult_ViewMore > span'
 
   // Retrieves the list of results
-  // return Array.from(document.querySelectorAll(ELEMENTS)).reduce((obj, result, index) => {
   return Array.from(document.querySelectorAll(ELEMENTS)).map(result => {
     const authors = result.querySelector(AUTHORS)
     const titleElement = result.querySelector(TITLE) || result.querySelector('h2 > span')
@@ -25,11 +22,11 @@ function createJSON () {
       title: titleElement.innerText,
       year: result.querySelector(YEAR).innerText.slice(6),
       abstract: abstract ? abstract.innerText : '',
-      authors: authors ? Array.prototype.map.call(authors.querySelectorAll('a > span'), author => author.innerText) : '',
+      authors: authors ? Array.prototype.map.call(authors.querySelectorAll('a > span'), author => author.innerText) : [],
       journal: result.querySelector(JOURNAL).innerText,
       document: document ? document.slice(10, -1) : '' // removes '/document/ and the trailing '/'
     }
   })
 }
 
-createJSON()
+module.exports = createJSON
---
+++ src/lib/json2xls.js
@@ -0,0 +1,36 @@
+const xl = require('excel4node')
+const path = require('path')
+
+const IEEEURL = 'https://ieeexplore.ieee.org/document/'
+
+/**
+ * Creates an excel file on disk based on an array of results.
+ *
+ * @param  {object[]]}  results      The results from scrapping IEEE, each result is an Object inside this array.
+ * @param  {string}     xlsFilename  The path and filename where to save the Excel file.
+ */
+function excel (results, xlsFilename) {
+  const wb = new xl.Workbook()
+  const ws = wb.addWorksheet('Sheet 1')
+
+  // ws.cell(row, col)
+  ws.cell(1, 1).string('YEAR').style({ font: { bold: true } })
+  ws.cell(1, 2).string('TITLE').style({ font: { bold: true } })
+  ws.cell(1, 3).string('AUTHORS').style({ font: { bold: true } })
+  ws.cell(1, 4).string('JOURNAL').style({ font: { bold: true } })
+  ws.cell(1, 5).string('ABSTRACT').style({ font: { bold: true } })
+  ws.cell(1, 6).string('IEEE URL').style({ font: { bold: true } })
+
+  for (let i = 0; i < results.length; i++) {
+    ws.cell(i + 2, 1).number(parseInt(results[i].year))
+    ws.cell(i + 2, 2).string(results[i].title)
+    ws.cell(i + 2, 3).string(results[i].authors.join('; '))
+    ws.cell(i + 2, 4).string(results[i].journal)
+    ws.cell(i + 2, 5).string(results[i].abstract)
+    if (results[i].document !== '') ws.cell(i + 2, 6).link(IEEEURL + results[i].document)
+  }
+  xlsFilename = path.parse(xlsFilename)
+  wb.write(path.join(xlsFilename.dir, xlsFilename.name + '.xls'))
+}
+
+module.exports = excel
