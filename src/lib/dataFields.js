77e4f6a5228600aaaf7fb9cf966f5d39109d137b
Felipe Castillo <fcastillo.ec@gmail.com>
2020-06-10 00:02:40 -0700

    style: improve data field function

--- src/lib/dataFields.js
+++ src/lib/dataFields.js
@@ -1,7 +1,16 @@
 /**
+ * IEEE Data Fields
+ */
+const FIELDS = {
+  fullTextAndMetadata: '"Full Text .AND. Metadata"',
+  textOnly: '"Full Text Only"',
+  publicationTitle: '"Publication Title"',
+  metadata: '"All Metadata"',
+  ieeeTerms: '"IEEE Terms"'
+}
+
+/**
  * Appends an specific data field to each search term.
- *
- * @private
  *
  * @param   {string}  querytext  the search string.
  * @param   {string}  field      the data field to append.
@@ -9,6 +18,8 @@
  * @return  {string}             the new search string with the data field appended.
  */
 function addDataField (querytext, field) {
+  if (!field) return querytext
+
   const operators = ['AND', 'OR', 'NOT']
   const near = new RegExp('O?NEAR/[0-9]*')
 
@@ -20,70 +31,7 @@ function addDataField (querytext, field) {
   return terms.join(' ')
 }
 
-/**
- * Searchs all Full Text & Metadata.
- *
- * @param   {string}  querytext  the search string.
- *
- * @return  {string}             the new search string.
- */
-function fullTextAndMetadata (querytext) {
-  // const textAndMetadata = '"Full Text & Metadata"'
-  const textAndMetadata = '"Full Text .AND. Metadata"'
-  return addDataField(querytext, textAndMetadata)
-}
-
-/**
- * Searchs all Full Text Only.
- *
- * @param   {string}  querytext  the search string.
- *
- * @return  {string}             the new search string.
- */
-function textOnly (querytext) {
-  return addDataField(querytext, '"Full Text Only"')
-}
-
-/**
- * Searchs all IEEE Terms.
- *
- * @param   {string}  querytext  the search string.
- *
- * @return  {string}             the new search string.
- */
-function ieeeTerms (querytext) {
-  return addDataField(querytext, '"IEEE Terms"')
-}
-
-/**
- * Searchs all Metadata.
- *
- * @param   {string}  querytext  the search string.
- *
- * @return  {string}             the new search string.
- */
-function metadata (querytext) {
-  return addDataField(querytext, '"All Metadata"')
-}
-
-/**
- * Searchs only publication title
- *
- * @param   {string}  querytext  the search string.
- *
- * @return  {string}             the new search string.
- */
-function publication (querytext) {
-  const pubTitle = '"Publication Title"'
-  if (querytext.startsWith('(')) return `(${pubTitle}:${querytext.slice(1)}`
-
-  return `${pubTitle}:${querytext}`
-}
-
 module.exports = {
-  textOnly,
-  fullTextAndMetadata,
-  ieeeTerms,
-  metadata,
-  publication
+  FIELDS,
+  addDataField
 }
