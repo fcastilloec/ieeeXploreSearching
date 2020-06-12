7bb744e3d1649782e85f03547cda8325512f6770
Felipe Castillo <fcastillo.ec@gmail.com>
2020-06-12 02:46:07 -0700

    feat(logic): use standarize json for logic operators
    A result_1 is considered equal to result_2 when both of their article numbers are equal. This
    is the unique identifier used by IEEE. If an article number is not present for any of the
    results, their "title", "publication_year", "content_type", and "publisher" have to be equal.
    
    BREAKING CHANGE: Search results from both API and Scrap can now be mixed when performing logic
      operations.

--- src/lib/logicOperations.js
+++ src/lib/logicOperations.js
@@ -1,7 +1,23 @@
 const _ = require('lodash')
 const fs = require('fs-extra')
 const { filename } = require('./utils')
-const json2xls = require('./json2xls')
+const { fromResults: json2xls } = require('./json2xls')
+
+/**
+ * Compares if array elements (results) are equal. Scrapping ONLY
+ *
+ * @param   {object}  value  The result to compare
+ * @param   {object}  other  The other result to compare
+ *
+ * @return  {boolean}        Returns true if both results are equal based on document number, or title and year
+ */
+function isEqual (value, other) {
+  if (value.article_number && other.article_number) return value.article_number === other.article_number
+  return (value.title === other.title) &&
+    (value.publication_year === other.publication_year) &&
+    (value.content_type === other.content_type) &&
+    (value.publisher === other.publisher)
+}
 
 /**
  * Perfom any of the logic operations among JSON files and saves them
@@ -16,20 +32,20 @@ function operations (options) {
     // MERGE or OR
     if (Object.prototype.hasOwnProperty.call(options, 'merge') || Object.prototype.hasOwnProperty.call(options, 'or')) {
       const files = (options.merge || options.or).map(file => fs.readJsonSync(file))
-      result = _.unionWith(...files, _.isEqual)
+      result = _.unionWith(...files, isEqual)
     }
 
     // AND
     if (Object.prototype.hasOwnProperty.call(options, 'and')) {
       const files = options.and.map(file => fs.readJsonSync(file))
-      result = _.intersectionWith(...files, _.isEqual)
+      result = _.intersectionWith(...files, isEqual)
     }
 
     // NOT
     if (Object.prototype.hasOwnProperty.call(options, 'not')) {
       const file = fs.readJsonSync(options.not)
       if (options._[0]) result = fs.readJsonSync(options._[0])
-      result = _.differenceWith(result, file, _.isEqual)
+      result = _.differenceWith(result, file, isEqual)
     }
   } catch (error) {
     console.error('Error reading JSON file:\n' + error.message)
@@ -43,7 +59,7 @@ function operations (options) {
     process.exit(6)
   }
   const outputfile = filename(options.output, '.xls')
-  if (options.excel) options.api ? json2xls.fromAPI(result, outputfile) : json2xls.fromScrapping(result, outputfile)
+  if (options.excel) json2xls(result, outputfile)
 }
 
 module.exports = operations
