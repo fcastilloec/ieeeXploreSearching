6b3b60172b6cd11934cdafee860d2a3c652dea78
Felipe Castillo <fcastillo.ec@gmail.com>
2020-06-17 19:28:53 -0700

    style: switch to airbnb eslint
    Preparing the code to be used with React

--- src/cli-search.js
+++ src/cli-search.js
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
-const _ = require('lodash')
-const fs = require('fs-extra')
-const yargs = require('yargs')
-const { filename, yearRange } = require('./lib/utils')
-const { FIELDS, addDataField } = require('./lib/dataFields')
-const ieee = require('./lib/ieeeAPI')
-const { fromResults: json2xls } = require('./lib/json2xls')
+const _ = require('lodash');
+const fs = require('fs-extra');
+const yargs = require('yargs');
+const { changeFileExtension, yearRange } = require('./lib/utils');
+const { FIELDS, addDataField } = require('./lib/dataFields');
+const ieee = require('./lib/ieeeAPI');
+const { fromResults: json2xls } = require('./lib/json2xls');
 
-const { APIKEY } = process.env
+const { APIKEY } = process.env;
 
-const argv = yargs
+const { argv } = yargs
   .wrap(null)
   .version(require('../package').version)
   .usage('Usage: $0 <query> [options] [IEEE Data Fields]')
@@ -21,115 +21,124 @@ const argv = yargs
     describe: 'Filename where to save results as JSON',
     nargs: 1,
     type: 'string',
-    demandOption: true
+    demandOption: true,
   })
   .option('full-text-and-metadata', {
     alias: 'f',
     conflicts: ['text-only', 'publication-title', 'metadata', 'ieee-terms'],
     describe: 'Searches for "Full Text & Metadata"',
-    type: 'boolean'
+    type: 'boolean',
   })
   .option('text-only', {
     alias: 't',
     conflicts: ['full-text-and-metadata', 'publication-title', 'metadata', 'ieee-terms'],
     describe: 'Searches for "Full Text Only"',
-    type: 'boolean'
+    type: 'boolean',
   })
   .option('publication-title', {
     alias: 'p',
     conflicts: ['text-only', 'full-text-and-metadata', 'metadata', 'ieee-terms'],
     describe: 'Searches for "Publication Title"',
-    type: 'boolean'
+    type: 'boolean',
   })
   .option('metadata', {
     alias: 'm',
     describe: 'Searches for "All Metadata"',
     conflicts: ['text-only', 'full-text-and-metadata', 'publication-title', 'ieee-terms'],
-    type: 'boolean'
+    type: 'boolean',
   })
   .option('ieee-terms', {
     alias: 'i',
     describe: 'Searches for "IEEE Terms"',
     conflicts: ['text-only', 'full-text-and-metadata', 'publication-title', 'metadata'],
-    type: 'boolean'
+    type: 'boolean',
   })
   .option('excel', {
     alias: 'e',
     describe: 'Whether to also save results as excel file in addition to JSON',
     default: false,
-    type: 'boolean'
+    type: 'boolean',
   })
   .option('year', {
     alias: 'y',
     nargs: 1,
     demandOption: true,
     describe: 'Year range to search. A single value will search from "year" to date',
-    type: 'number'
+    type: 'number',
   })
   .option('api', {
     alias: 'a',
     describe: 'Whether to use the IEEE API or scrapping',
     default: false,
-    type: 'boolean'
+    type: 'boolean',
   })
   .option('verbose', {
     alias: 'v',
     describe: 'Show extra info',
-    type: 'boolean'
+    type: 'boolean',
   })
   .parserConfiguration({
     'strip-aliased': true,
-    'strip-dashed': true
+    'strip-dashed': true,
   })
-  .check(argv => {
-    if (argv.year.length > 2) throw new Error('Only start and/or finish year are accepted')
-    if (Array.isArray(argv.year)) {
-      for (let i = 0; i < 2; i++) {
-        if (!Number.isInteger(argv.year[i])) throw new Error('Year has to be a integer')
-        if (argv.year[i].toString().length !== 4) throw new Error('Year has to be a 4 digit integer')
-        if (argv.year[i] < 1900 || argv.year[i] > new Date().getFullYear()) throw new Error('Year has to be after 1900 and before current one')
-      }
+  .check((args) => {
+    if (args.year.length > 2) throw new Error('Only start and/or finish year are accepted');
+    if (Array.isArray(args.year)) {
+      args.year.forEach((year) => {
+        if (!Number.isInteger(year)) throw new Error('Year has to be a integer');
+        if (year.toString().length !== 4) throw new Error('Year has to be a 4 digit integer');
+        if (year < 1900 || year > new Date().getFullYear()) {
+          throw new Error('Year has to be after 1900 and before current one');
+        }
+      });
     } else {
-      if (!Number.isInteger(argv.year)) throw new Error('Year has to be a integer')
-      if (argv.year.toString().length !== 4) throw new Error('Year has to be a 4 digit integer')
-      if (argv.year < 1900 || argv.year > new Date().getFullYear()) throw new Error('Year has to be after 1900 and before current one')
+      if (!Number.isInteger(args.year)) throw new Error('Year has to be a integer');
+      if (args.year.toString().length !== 4) throw new Error('Year has to be a 4 digit integer');
+      if (args.year < 1900 || args.year > new Date().getFullYear()) {
+        throw new Error('Year has to be after 1900 and before current one');
+      }
     }
-    if (argv.api && !APIKEY) throw new Error('No APIKEY key provided. Set APIKEY enviroment variable')
-    return true
+    if (args.api && !APIKEY) throw new Error('No APIKEY key provided. Set APIKEY enviroment variable');
+    return true;
   })
   .group(['full-text-and-metadata', 'text-only', 'publication-title', 'metadata', 'ieee-terms'], 'IEEE Data Fields')
-  .example('$0 "optics AND nano" -o search1 -y 1990 -y 2000 -e', 'searches for "optics AND nano" between 1990-2000 and save the results in search1.json and search1.xls')
-  .example('$0 "h264 NEAR/3 cellular" -y 2005 -o search2.json', 'searches for "h264 NEAR/3 cellular" from 2005 to date, and save the results in search2.json')
-  .argv
+  .example(
+    '$0 "optics AND nano" -o search1 -y 1990 -y 2000 -e',
+    'searches for "optics AND nano" between 1990-2000 and save the results in search1.json and search1.xls',
+  )
+  .example(
+    '$0 "h264 NEAR/3 cellular" -y 2005 -o search2.json',
+    'searches for "h264 NEAR/3 cellular" from 2005 to date, and save the results in search2.json',
+  );
 
 // Selects the enabled key of the specified data field. _.pick is faster than _.omit
-const dataField = Object.keys(_.pick(argv, Object.keys(FIELDS)))[0]
+const dataField = Object.keys(_.pick(argv, Object.keys(FIELDS)))[0];
 // Transform the year into an array if only one was specified
-const rangeYear = yearRange(argv.year)
+const rangeYear = yearRange(argv.year);
 
-console.log('Searching for: %s', argv._[0])
-console.log('Between %s and %s', rangeYear[0], rangeYear[1])
-console.log('Using: %s', FIELDS[dataField] || 'No data fields')
+console.log('Searching for: %s', argv._[0]);
+console.log('Between %s and %s', rangeYear[0], rangeYear[1]);
+console.log('Using: %s', FIELDS[dataField] || 'No data fields');
 
-async function search () {
+async function search() {
   const results = argv.api
     ? await ieee.api(APIKEY, addDataField(argv._[0], FIELDS[dataField]), rangeYear, argv.verbose)
-    : await ieee.scrap(addDataField(argv._[0], FIELDS[dataField]), rangeYear, argv.verbose)
+    : await ieee.scrap(addDataField(argv._[0], FIELDS[dataField]), rangeYear, argv.verbose);
 
-  console.log('Found %s results', results.total_records)
+  console.log('Found %s results', results.total_records);
   if (argv.api && results.total_records > 200) {
-    console.warn('WARNING: API searches are limited to fetching 200 results')
-    console.warn('\t Consider using scraping or narrowing your search')
+    console.warn('WARNING: API searches are limited to fetching 200 results');
+    console.warn('\t Consider using scraping or narrowing your search');
   }
   if (results.total_records > 0) {
     try {
-      await fs.ensureFile(filename(argv.output, '.json')) // create the parent directory if they don't exist
-      await fs.writeJson(filename(argv.output, '.json'), results.articles, { spaces: 1 })
+      await fs.ensureFile(changeFileExtension(argv.output, '.json')); // create the parent directory if they don't exist
+      await fs.writeJson(changeFileExtension(argv.output, '.json'), results.articles, { spaces: 1 });
     } catch (error) {
-      console.error('Error writing JSON file:\n' + error)
-      process.exit(6)
+      console.error(`Error writing JSON file:\n${error}`);
+      process.exit(6);
     }
-    if (argv.excel) json2xls(results.articles, filename(argv.output, '.xls'))
+    if (argv.excel) json2xls(results.articles, changeFileExtension(argv.output, '.xls'));
   }
 }
-search()
+search();
--- src/lib/dataFields.js
+++ src/lib/dataFields.js
@@ -6,8 +6,8 @@ const FIELDS = {
   textOnly: '"Full Text Only"',
   publicationTitle: '"Publication Title"',
   metadata: '"All Metadata"',
-  ieeeTerms: '"IEEE Terms"'
-}
+  ieeeTerms: '"IEEE Terms"',
+};
 
 /**
  * Appends an specific data field to each search term.
@@ -17,25 +17,26 @@ const FIELDS = {
  *
  * @return  {string}             the new search string with the data field appended.
  */
-function addDataField (querytext, field) {
-  if (!field) return querytext
+function addDataField(querytext, field) {
+  if (!field) return querytext;
 
-  const operators = ['AND', 'OR', 'NOT']
-  const near = new RegExp('O?NEAR/[0-9]*')
+  const operators = ['AND', 'OR', 'NOT'];
+  const near = new RegExp('O?NEAR/[0-9]*');
 
-  const terms = querytext.split(' ').map(term => {
-    if (operators.includes(term) || term.match(near)) return term
+  const terms = querytext.split(' ').map((term) => {
+    if (operators.includes(term) || term.match(near)) return term;
 
     // By spliting the term on '(', we can find opening parenthesis as empty array elements.
-    return term.split('(').reduce((outputString, elem) => {
-      // Re-adds a parenthesis for empty elements, and appends the field to non-empty ones.
-      return !elem ? outputString + '(' : outputString + field + ':' + elem
-    }, '') // Initial value for outputString is empty string ''
-  })
-  return terms.join(' ')
+    return term.split('(').reduce(
+      (outputString, elem) => (!elem ? `${outputString}(` : `${outputString + field}:${elem}`),
+      '',
+    ); // Initial value for outputString is empty string ''
+  });
+  return terms.join(' ');
 }
+// Re-adds a parenthesis for empty elements, and appends the field to non-empty ones.
 
 module.exports = {
   FIELDS,
-  addDataField
-}
+  addDataField,
+};
