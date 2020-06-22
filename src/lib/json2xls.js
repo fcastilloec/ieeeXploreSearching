d8b32017df6e4c3bb9332fea62e2fc97b0eecc4c
Felipe Castillo <fcastillo.ec@gmail.com>
2020-06-21 21:38:19 -0700

    refactor: move write-to-file operations to cli

--- src/lib/json2xls.js
+++ src/lib/json2xls.js
@@ -1,6 +1,4 @@
 const xl = require('excel4node');
-const fs = require('fs-extra');
-const { changeFileExtension } = require('./utils');
 
 /**
  * Converts an array of authors into a string.
@@ -88,53 +86,40 @@ function fromResults(results, xlsFilename) {
   ws.cell(1, COLUMNS.pdf_url).string('IEEE URL').style({ font: { bold: true } });
   ws.cell(1, COLUMNS.doi).string('SCI-HUB URL').style({ font: { bold: true } });
 
-  for (let i = 0; i < results.length; i++) {
+  results.forEach((result, i) => {
     ws.row(i + 2).setHeight(97);
-    ws.cell(i + 2, COLUMNS.publication_year).number(parseInt(results[i].publication_year, 10)).style(noWrapStyle);
-    ws.cell(i + 2, COLUMNS.article_number).string(results[i].article_number).style(noWrapStyle);
-    if (results[i].publication_date) {
+    ws.cell(i + 2, COLUMNS.publication_year).number(parseInt(result.publication_year, 10)).style(noWrapStyle);
+    ws.cell(i + 2, COLUMNS.article_number).string(result.article_number).style(noWrapStyle);
+    if (result.publication_date) {
       pubDate = false;
-      ws.cell(i + 2, COLUMNS.publication_date).string(results[i].publication_date).style(noWrapStyle);
+      ws.cell(i + 2, COLUMNS.publication_date).string(result.publication_date).style(noWrapStyle);
     }
-    ws.cell(i + 2, COLUMNS.title).string(`${results[i].title}\n`).style(wrapStyle).style(wrapStyle);
-    ws.cell(i + 2, COLUMNS.authors).string(`${authorsString(results[i].authors.authors)}\n`).style(wrapStyle);
-    ws.cell(i + 2, COLUMNS.publication_title).string(`${results[i].publication_title || ''}\n`).style(wrapStyle);
-    ws.cell(i + 2, COLUMNS.abstract).string(`${results[i].abstract || ''}\n`).style(wrapStyle);
-    ws.cell(i + 2, COLUMNS.pdf_url).link(results[i].pdf_url || results[i].abstract_url || '').style(linkStyle);
-    if (results[i].doi) {
+    ws.cell(i + 2, COLUMNS.title).string(`${result.title}\n`).style(wrapStyle).style(wrapStyle);
+    ws.cell(i + 2, COLUMNS.authors).string(`${authorsString(result.authors.authors)}\n`).style(wrapStyle);
+    ws.cell(i + 2, COLUMNS.publication_title).string(`${result.publication_title || ''}\n`).style(wrapStyle);
+    ws.cell(i + 2, COLUMNS.abstract).string(`${result.abstract || ''}\n`).style(wrapStyle);
+    ws.cell(i + 2, COLUMNS.pdf_url).link(result.pdf_url || result.abstract_url || '').style(linkStyle);
+    if (result.doi) {
       doi = false;
-      ws.cell(i + 2, COLUMNS.doi).link(sciHubUrl + results[i].doi).style(linkStyle);
+      ws.cell(i + 2, COLUMNS.doi).link(sciHubUrl + result.doi).style(linkStyle);
     }
-  }
+  });
 
+  /* istanbul ignore next */
   if (pubDate) ws.column(COLUMNS.publication_date).hide();
+  /* istanbul ignore next */
   if (doi) ws.column(COLUMNS.doi).hide();
 
   return new Promise((resolve, reject) => {
     wb.write(xlsFilename, (error) => {
+      /* istanbul ignore next */
       if (error) return reject(new Error(`Error writing xls file to disk\n${error.message}`));
       return resolve();
     });
   });
 }
 
-/**
- * Creates an excel file from a JSON file.
- *
- * @param  {string}  file      The JSON file to convert.
- */
-async function fromFile(file) {
-  let results;
-  const output = changeFileExtension(file, 'xls');
-  try {
-    results = fs.readJsonSync(file);
-  } catch (error) {
-    throw new Error(`Error reading JSON file:\n${error.message}`);
-  }
-  await fromResults(results, output);
-}
-
 module.exports = {
-  fromFile,
+  authorsString,
   fromResults,
 };
