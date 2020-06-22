953702390aad80515277bcfe03770ca0c6bc01b2
Felipe Castillo <fcastillo.ec@gmail.com>
2020-06-21 21:38:19 -0700

    feat: move writing operations outside of lib functions

--- src/lib/logicOperations.js
+++ src/lib/logicOperations.js
@@ -1,7 +1,5 @@
 const _ = require('lodash');
 const fs = require('fs-extra');
-const { changeFileExtension } = require('./utils');
-const { fromResults } = require('./json2xls');
 
 /**
  * Compares if array elements (results) are equal. Scrapping ONLY
@@ -25,43 +23,34 @@ function isEqual(value, other) {
  *
  * @param   {object}  options  The options passed from command-line
  */
-function operations(options) {
-  let result;
+/* istanbul ignore next */
+async function logicOperations(options) {
+  let result = [];
 
-  try {
-    // MERGE or OR
-    if ('merge' in options || 'or' in options) {
-      const files = (options.merge || options.or).map((file) => fs.readJsonSync(file));
-      result = _.unionWith(...files, isEqual);
-    }
-
-    // AND
-    if ('and' in options) {
-      const files = options.and.map((file) => fs.readJsonSync(file));
-      result = _.intersectionWith(...files, isEqual);
-    }
-
-    // NOT
-    if ('not' in options) {
-      const file = fs.readJsonSync(options.not);
-      if (options._[0]) result = fs.readJsonSync(options._[0]);
-      result = _.differenceWith(result, file, isEqual);
-    }
-  } catch (error) {
-    throw new Error(`Error reading JSON file:\n${error.message}`);
+  // MERGE or OR
+  if ('merge' in options || 'or' in options) {
+    const files = (options.merge || options.or).map((file) => fs.readJsonSync(file));
+    result = _.unionWith(...files, isEqual);
   }
 
-  if (result.length > 0) {
-    console.log('Operation returned %s results', result.length);
-    try {
-      fs.writeJsonSync(changeFileExtension(options.output, '.json'), result, { spaces: 1 });
-    } catch (error) {
-      throw new Error(`Error writing JSON file:\n${error.message}`);
-    }
-    if (options.excel) fromResults(result, changeFileExtension(options.output, '.xls'));
-  } else {
-    console.log('Logic operation returned zero results. No files will be saved.');
+  // AND
+  if ('and' in options) {
+    const files = options.and.map((file) => fs.readJsonSync(file));
+    result = _.intersectionWith(...files, isEqual);
   }
+
+  // NOT
+  if ('not' in options) {
+    const file = fs.readJsonSync(options.not);
+    // Read NOT file if not used in combination with other operators
+    if (options._[0]) result = fs.readJsonSync(options._[0]);
+    result = _.differenceWith(result, file, isEqual);
+  }
+
+  return result;
 }
 
-module.exports = operations;
+module.exports = {
+  logicOperations,
+  isEqual,
+};
