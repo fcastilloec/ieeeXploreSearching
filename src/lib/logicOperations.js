abfef7b7def7e9a92f99007fd8d1333cfc88db22
Felipe Castillo <fcastillo.ec@gmail.com>
2020-06-24 23:24:10 -0700

    feat: logic operators are boolean instead of array

--- src/lib/logicOperations.js
+++ src/lib/logicOperations.js
@@ -27,24 +27,20 @@ function isEqual(value, other) {
 function logicOperations(options) {
   let result = [];
 
+  const files = options._.map((file) => fs.readJsonSync(file));
+
   // MERGE or OR
-  if ('merge' in options || 'or' in options) {
-    const files = (options.merge || options.or).map((file) => fs.readJsonSync(file));
-    result = _.unionWith(...files, isEqual);
-  }
+  if (options.merge || options.or) result = _.unionWith(...files, isEqual);
 
   // AND
-  if ('and' in options) {
-    const files = options.and.map((file) => fs.readJsonSync(file));
-    result = _.intersectionWith(...files, isEqual);
-  }
+  if (options.and) result = _.intersectionWith(...files, isEqual);
 
   // NOT
-  if ('not' in options) {
-    const file = fs.readJsonSync(options.not);
-    // Read NOT file if not used in combination with other operators
-    if (options._[0]) result = fs.readJsonSync(options._[0]);
-    result = _.differenceWith(result, file, isEqual);
+  if (options.not) {
+    const notFile = fs.readJsonSync(options.not);
+    result = options.merge || options.or || options.and
+      ? _.differenceWith(result, notFile, isEqual) // use previous results
+      : _.differenceWith(...files, notFile, isEqual); // only use provided files
   }
 
   return result;
