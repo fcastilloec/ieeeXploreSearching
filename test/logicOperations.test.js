2395b4eac58f355bd696a8c1c454b37686781bc2
Felipe Castillo <1280530+fcastilloec@users.noreply.github.com>
2021-06-06 11:48:32 -0700

    test: switch to jest (#149)
    This is in preparation for scrap testing

--- test/logicOperations.test.js
+++ test/logicOperations.test.js
@@ -1,14 +1,13 @@
-const test = require('ava');
 const { isEqual } = require('../src/lib/logicOperations');
 
-test('isEqual with article_number', (t) => {
+test('isEqual with article_number', () => {
   const value1 = { article_number: 1 };
-  t.true(isEqual(value1, value1));
+  expect(isEqual(value1, value1)).toBeTruthy();
 });
 
-test('isEqual without article_number', (t) => {
+test('isEqual without article_number', () => {
   const value2 = {
     title: 'title', publication_year: 2000, content_type: 'type', publisher: 'publisher',
   };
-  t.true(isEqual(value2, value2));
+  expect(isEqual(value2, value2)).toBeTruthy();
 });
---
+++ test/utils.test.js
@@ -0,0 +1,37 @@
+const { changeFileExtension, testFileExtension, testYear } = require('../src/lib/utils');
+
+const path = 'this/file.ext';
+const out1 = 'this/file.new';
+const out2 = 'this/file.ext.new';
+
+test('changeFileExtension without period', () => {
+  expect(changeFileExtension(path, 'new')).toBe(out1);
+});
+
+test('changeFileExtension with period', () => {
+  expect(changeFileExtension(path, '.new')).toBe(out1);
+});
+
+test('testFileExtension without period', () => {
+  expect(testFileExtension(path, 'new')).toBe(out2);
+});
+
+test('testFileExtension with same extension', () => {
+  expect(testFileExtension(path, '.ext')).toBe(path);
+});
+
+test('testYear not throws for valid year', () => {
+  expect(() => testYear(1990)).not.toThrow();
+});
+
+test('testYear throws if not an integer', () => {
+  expect(() => testYear(15.4)).toThrow('Year has to be an integer');
+});
+
+test('testYear throws before 1900', () => {
+  expect(() => testYear(0)).toThrow('Year option has to be after 1900');
+});
+
+test('testYear throws after current year', () => {
+  expect(() => testYear(1e14)).toThrow('Year option has to be before current year');
+});
