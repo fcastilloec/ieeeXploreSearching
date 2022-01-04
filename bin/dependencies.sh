400a5624c95322da2b98f88af75a91f406eb9562
Felipe Castillo <fcastillo.ec@gmail.com>
2022-01-04 12:51:20 -0800

    build: fix script for creating Dependencies section in changelog

--- bin/dependencies.sh
+++ bin/dependencies.sh
@@ -1,25 +1,22 @@
 #!/usr/bin/env bash
 
-set -eE
-failure() {
-  local lineno=$1; local msg=$2; echo "$(basename "$0"): failed at ${lineno}: ${msg}"
-}
-trap 'failure ${LINENO} "$BASH_COMMAND"' ERR
-
 # VARIABLES
 DIR=$(dirname "$0")
 changelog="${DIR}/../CHANGELOG.md"
-gron="${DIR}/gron-x64"
+gron="${DIR}/gron-$(arch)"
 tag=$(git describe --tags --abbrev=0)
 
 # Get dependencies that have changed since last tag
-dependenciesJSON=$(diff --changed-group-format='%>'" "--unchanged-group-format=''\
+# If no changes found, it will return a non-zero value
+dependenciesJSON=$(diff --changed-group-format='%>' --unchanged-group-format=''\
  <(git show "${tag}:package.json" | ${gron}) <(${gron} package.json)\
  | grep dependencies\
  | ${gron} --ungron)
 
-# Format text for changelog
-replaceText=$(echo "${dependenciesJSON}" | jq -r '.dependencies | to_entries[] | "* bump `\(.key)` to `\(.value)`" | @text')
+if [[ -n ${dependenciesJSON} ]]; then
+  # Format text for changelog
+  replaceText=$(echo "${dependenciesJSON}" | jq -r '.dependencies | to_entries[] | "* bump `\(.key)` to `\(.value)`" | @text')
 
-# Replace the latest Dependencies section in the Changelog
-npx --no markdown-replace-section "${changelog}" Dependencies "${changelog}" --not-hungry <<< "${replaceText//[~^]/}"
+  # Replace the latest Dependencies section in the Changelog
+  npx --no markdown-replace-section "${changelog}" Dependencies "${changelog}" --not-hungry <<< "${replaceText//[~^]/}"
+fi
