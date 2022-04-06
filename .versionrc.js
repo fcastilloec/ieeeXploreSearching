929df85ca6d007bbc425bc725c0f1742ee5207dd
Felipe Castillo <fcastillo.ec@gmail.com>
2022-04-05 18:31:42 -0700

    build: fix standard version commit message

--- .versionrc.js
+++ .versionrc.js
@@ -1,10 +1,11 @@
 module.exports = {
   types: [
-    { type: 'build', section: 'Dependencies' },
+    { type: 'chore', section: 'Dependencies' },
     { type: 'feat', section: 'Features' },
     { type: 'fix', section: 'Bug Fixes' },
   ],
   scripts: {
     postchangelog: './bin/dependencies.sh',
   },
+  releaseCommitMessageFormat: 'build(release): {{currentTag}}'
 }
